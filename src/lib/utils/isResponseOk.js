066c38fcdf4e72dcaefa2e0852be6e6a6e7f93ef
quentincaffeino <sergey95zu@gmail.com>
2022-02-12 19:09:22 +0100

    chores: more verbose response check

--- src/lib/utils/isResponseOk.js
+++ src/lib/utils/isResponseOk.js
@@ -4,5 +4,5 @@
  */
 export default function isResponseOk(response) {
   if (response.ok) return response;
-  else throw { error: "response is not ok", response };
+  else throw { error: `response from '${response.url}' is not ok`, response };
 }
