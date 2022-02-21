e7adcdafe5c39137f0816aee9c41cdb3b38979e0
quentincaffeino <sergey95zu@gmail.com>
2022-02-22 00:17:53 +0100

    fix: forgot blocks index file

---
+++ src/lib/blocks/index.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+  import * as projects from "./projects";
+  import * as topartists from "./topartists";
+
+  export const blocksMap = new Map();
+  blocksMap.set("projects", projects);
+  blocksMap.set("topartists", topartists);
+
+  function getBlockComponent(name) {
+    return blocksMap.get(name)?.component;
+  }
+</script>
+
+<script>
+  export let name;
+  export let props;
+</script>
+
+<svelte:component this={getBlockComponent(name)} {...props} />
